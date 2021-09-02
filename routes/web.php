63e2e874eb086aaaca82c60c65b446bac1e5e3ef
Wilson Santos <contato@wilsonsantos.net>
2021-09-02 16:21:59 -0300

    :sparkles: Implementa o login
      Auth do proprio laravel
      restringe urls de ação

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,22 @@ Route::get('/', function () {
 });
 
 Route::get('/matches','MatchesController@index')->name('list_matches');
-Route::get('/matches/create','MatchesController@create')->name('form_match_create');;
+Route::get('/matches/create','MatchesController@create')
+    ->name('form_match_create')
+    ->middleware('auth');
 Route::get('/matches/{id}','MatchesController@show')->name('show_match');
-Route::post('/matches/create','MatchesController@store');
-route::get('/matches/update/{id}','MatchesController@formUpdate')->name('form_match_update');
-Route::patch('/matches/{id}','MatchesController@update')->name('update_match');
-Route::delete('/matches/remove/{id}','MatchesController@destroy')->name('match_remove');
+Route::post('/matches/create','MatchesController@store')
+    ->middleware('auth');;
+route::get('/matches/update/{id}','MatchesController@formUpdate')
+    ->name('form_match_update')
+    ->middleware('auth');
+Route::patch('/matches/{id}','MatchesController@update')
+    ->name('update_match')
+    ->middleware('auth');
+Route::delete('/matches/remove/{id}','MatchesController@destroy')
+    ->name('match_remove')
+    ->middleware('auth');
+
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
+
